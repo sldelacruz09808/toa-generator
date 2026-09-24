aa192b0c92c0251deb8c802e85902cd7498344a0
Bri808 <sldelacruz09@gmail.com>
2026-09-24 09:43:08 -1000

    Add files via upload

--- license-check.js
+++ license-check.js
@@ -17,7 +17,7 @@
   'use strict';
 
   // ---- CONFIGURE THESE TWO VALUES ----
-  const CONFIG_URL = 'https://github.io';
+  const CONFIG_URL = 'https://yourdomain.com/access-control.json';
   const GRACE_PERIOD_MS = 5 * 24 * 60 * 60 * 1000; // 5 days
   // -------------------------------------
 
